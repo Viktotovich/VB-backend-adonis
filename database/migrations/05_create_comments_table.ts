31f4594dbd3a4de802a41b1e5445763fc70279dc
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-05-03 16:06:24 +0400

    Created post categories migration

---
+++ database/migrations/05_create_comments_table.ts
@@ -0,0 +1,25 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'comments'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.json('content').notNullable()
+
+      //References
+      table.integer('author_id').unsigned().references('user.id').notNullable().onDelete('CASCADE')
+      table.integer('blog_id').unsigned().references('blog.id').notNullable().onDelete('CASCADE')
+      table.string('image_url').nullable() // if later on users can images
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
