54deee15857924ea9b6ad9310501355fed47747d
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-05-03 18:34:47 +0400

    Wrote the necessary TODOs as a reminder.
    Register page partially handled,
    
    further work: Login + Validator
    
    Inquiry / Lead handler + Admin Panel + Lead Model / Table
    
    Haven't yet decided if the admin panel should live on the backend, with the safety and advantages of EdgeJS
    
    or on the frontend (most likely), where we can take full advantage of CDD + React

--- start/routes.ts
+++ start/routes.ts
@@ -18,3 +18,19 @@ router
   })
   .prefix('auth')
   .as('auth')
+
+/*
+  TODOs:
+  1 - Inquiry table without auth to receive people who come to the landing page and
+    fill out the forms
+
+  2 - Have the frontend change from form to thank you component on successful submit
+
+  3 - Rate limitor, never trust the client
+
+  4 - Inquiries in the admin view + email, have the backend send the emails to
+  clients with a generic text, thank you, and timeline + something useful
+
+  5 - Somehow ensure that only the frontend can send that data, probs CORS + 
+  some form of XSS protection
+*/
