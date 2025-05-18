e058509729a25a8a365d9dea1e652e2a329228b8
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-05-18 17:15:13 +0400

    Chore: implemented proper protection for dash controller
    1 >> Unauthed users dont waste resources
    
    2>> Unauthed users accessing the dashboard get booter

--- app/controllers/dashboard_controller.ts
+++ app/controllers/dashboard_controller.ts
@@ -3,6 +3,10 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class DashboardController {
   async show({ request, response, auth }: HttpContext) {
+    const user = auth.user
+
+    if (!user) return response.unauthorized()
+
     const page = request.input('page', 1)
     const limit = request.input('limit', 10)
 
@@ -13,8 +17,6 @@ export default class DashboardController {
       .preload('replies')
       .orderBy('createdAt', 'desc')
       .paginate(page, limit)
-
-    const user = auth.user!
     await user.load('likes')
     const userLikes = user.likes.map((like) => like.postId)
 
